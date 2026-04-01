1d88f77a32aebd55b3cb3d4ed012746990b18674
Mayun1988998 <yaoxinghe820@gmail.com>
2026-04-01 09:41:30 -0700

    Polish app dashboard and scrollbar behavior

--- recordings/mac_app/MatchPlanRecorderApp/Sources/MatchPlanRecorderApp/MatchPlanRecorderApp.swift
+++ recordings/mac_app/MatchPlanRecorderApp/Sources/MatchPlanRecorderApp/MatchPlanRecorderApp.swift
@@ -4,6 +4,12 @@ import SwiftUI
 struct MatchPlanRecorderApp: App {
     @State private var controller = AppController()
 
+    init() {
+        UserDefaults.standard.register(defaults: [
+            "AppleShowScrollBars": "WhenScrolling"
+        ])
+    }
+
     var body: some Scene {
         WindowGroup("MatchPlan Recorder") {
             ContentView(controller: controller)
